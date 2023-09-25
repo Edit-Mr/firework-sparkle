0b90fb0273d4fc2cc96a3b981c4b15b83bcb7623
Edit Mr <elvismao.070512@gmail.com>
2023-09-25 10:15:42 +0800

    更新 pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 function c (_0: number, _1: number, _2: number) {
     led.plotBrightness(2, 2, _0)
     let i;
-    for (i = 0; i <= 3; i++)led.plotBrightness(1, i + 1, _1)
-    for (i = 0; i <= 3; i++)led.plotBrightness(3, i + 1, _1)
-    led.plotBrightness(2, 1, _1)
+for (i = 0; i <= 3; i++)led.plotBrightness(1, i + 1, _1)
+for (i = 0; i <= 3; i++)led.plotBrightness(3, i + 1, _1)
+led.plotBrightness(2, 1, _1)
     led.plotBrightness(2, 3, _1)
     for (i = 0; i <= 4; i++)led.plotBrightness(0, i, _2)
-    for (i = 0; i <= 4; i++)led.plotBrightness(4, i, _2)
-    for (i = 0; i <= 4; i++)led.plotBrightness(i, 0, _2)
-    for (i = 0; i <= 4; i++)led.plotBrightness(i, 4, _2)
-    basic.pause(200)
+for (i = 0; i <= 4; i++)led.plotBrightness(4, i, _2)
+for (i = 0; i <= 4; i++)led.plotBrightness(i, 0, _2)
+for (i = 0; i <= 4; i++)led.plotBrightness(i, 4, _2)
+basic.pause(200)
 }
 basic.forever(function () {
     c(50, 0, 0)
